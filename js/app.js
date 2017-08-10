116f814bfd95cd0e156031e06f416428fbdcb3d1
Natasha Umer <natasha.umer2@gmail.com>
2017-08-10 14:10:28 -0400

    added left & right arrow key movements

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ window.onload = function() {
   document.getElementById("button").addEventListener("click", startEvent);
 
   var counter = 0
+  var goRight = 0
 
   let box1 = document.querySelector('.box1');
   let box2 = document.querySelector('.box2');
@@ -23,16 +24,36 @@ window.onload = function() {
 
   function startEvent() {
     document.addEventListener('keydown', (event) => {
+
       if (event.keyCode === 40) {
         counter += 10;
         document.querySelector('.box1').style.top = counter + "px";
        if(isCollision(box1,box2)){alert('collision')};
-      } // end if event.keycode
-    });
-  }
+      } // end if down event.keycode
+
+      if (event.keyCode === 38) {
+        counter -= 10;
+        document.querySelector('.box1').style.top = counter + "px";
+       if(isCollision(box1,box2)){alert('collision')};
+     } // end if 38 up statement
+
+     if (event.keyCode === 39) {
+        goRight += 10;
+        document.querySelector('.box1').style.left = goRight + "px";
+       if(isCollision(box1,box2)){alert('collision')};
+     } // end if 39 right statement, 37 to move left
+
+     if (event.keyCode === 37) {
+        goRight -= 10;
+        document.querySelector('.box1').style.left = goRight + "px";
+       if(isCollision(box1,box2)){alert('collision')};
+     } // end if 39 right statement, 37 to move left
+
+    }); // end event listner keydown event
+  } // end function start event
 
 
-}
+} // window onlaod
 
 
 
