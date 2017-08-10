2863c4730d5fe55443477d1ff8d2ab4548c140cc
Natasha Umer <natasha.umer2@gmail.com>
2017-08-10 14:52:20 -0400

    experimenting with set interval function

--- js/app.js
+++ js/app.js
@@ -2,13 +2,15 @@ window.onload = function() {
   console.log(document.getElementById('button'))
   document.getElementById("button").addEventListener("click", startEvent);
 
-  var counter = 0
-  var goRight = 0
 
   let box1 = document.querySelector('.box1');
   let box2 = document.querySelector('.box2');
 
-  function isCollision(b1,b2){
+  var counter = 0
+  var goRight = 0
+
+
+  function isCollision(){
     var box1Pos = box1.getBoundingClientRect();
     var box2Pos = box2.getBoundingClientRect();
 
@@ -16,13 +18,35 @@ window.onload = function() {
         box1Pos.top + box1Pos.width > box2Pos.top &&
        box1Pos.left < box2Pos.left + box2Pos.height &&
         box1Pos.height + box1Pos.left > box2Pos.left) {
-      alert('collision');
+      console.log('is collision')
+      return true;
     } else {
       return false
     }
   } // end is collision
 
+//function myFunction() {
+ //   setInterval(function(){ alert("Hello"); }, 3000);
+//}
+
+function alertMe() {
+  alert('hello')
+}
+
+
+
   function startEvent() {
+
+    //setInterval(function(){ alert("Hello"); }, 3000)
+
+  //setInterval(alertMe, 3000)
+
+  setInterval(isCollision, 300)
+
+
+     //setInterval(function(){ alert("Hello"); }, 3000)
+
+    //window.setInterval(isCollision(box1,box2), 100)
     document.addEventListener('keydown', (event) => {
 
       if (event.keyCode === 40) {
@@ -47,7 +71,7 @@ window.onload = function() {
         goRight -= 10;
         document.querySelector('.box1').style.left = goRight + "px";
        if(isCollision(box1,box2)){alert('collision')};
-     } // end if 39 right statement, 37 to move left
+     } // end if 37 left statement
 
     }); // end event listner keydown event
   } // end function start event
@@ -59,17 +83,5 @@ window.onload = function() {
 
 
 
-      //if (event.keyCode === 38) {
-      //  counter -= 10;
-      //  document.querySelector('.box1').style.top = counter + "px";
-      //  if (isCollision(box1,box2)){alert('collision')};
-      //}
 
-      // end if event keycode event
-
-    //}); // end keydown event
-  //} // end start Event
-
-
-//} // window onload end
 
